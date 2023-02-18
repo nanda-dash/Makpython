9c5aba22b826397d8e8a25ca264de972e726e209
Nanda DASH <nanda81@gmail.com>
2023-02-19 00:12:00 +0100

    Add files via upload
    xml to xlsx generator added
    xml to c source/header file generator added

---
+++ example.c
@@ -0,0 +1,11 @@
+#include "example.h"
+
+const uint32_t BatteryConfig_Brand = 1;
+const uint32_t BatteryConfig_Revision = 6;
+const uint32_t BatteryConfig_HardwareType = 25;
+const uint32_t BatteryConfig_CellBrand = 4;
+const uint32_t BatteryConfig_CellType = 0;
+const uint32_t LowPower_InactivityTime = 300;
+const uint32_t LowPower_InactivityTimeRTC = 10;
+const uint32_t BatterySupervision_ChargerOverCurrentTime = 10000;
+const uint32_t Communication_InactiveCommunicationTime = 6000;
---
+++ example.h
@@ -0,0 +1,14 @@
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+extern const uint32_t BatteryConfig_Brand;
+extern const uint32_t BatteryConfig_Revision;
+extern const uint32_t BatteryConfig_HardwareType;
+extern const uint32_t BatteryConfig_CellBrand;
+extern const uint32_t BatteryConfig_CellType;
+extern const uint32_t LowPower_InactivityTime;
+extern const uint32_t LowPower_InactivityTimeRTC;
+extern const uint32_t BatterySupervision_ChargerOverCurrentTime;
+extern const uint32_t Communication_InactiveCommunicationTime;
+
+#endif
